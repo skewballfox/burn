051ef87a9bf80c9b89baf35e855111d47aeb019a
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2026-04-19 11:32:00 -0500

    ndarray complex work

--- crates/burn-ndarray/src/ops/simd/mod.rs
+++ crates/burn-ndarray/src/ops/simd/mod.rs
@@ -3,6 +3,8 @@ mod base;
 pub(crate) mod binary;
 pub(crate) mod binary_elemwise;
 pub(crate) mod cmp;
+#[cfg(feature = "complex")]
+pub(crate) mod complex;
 pub(crate) mod conv;
 pub(crate) mod maxpool;
 pub(crate) mod unary;
